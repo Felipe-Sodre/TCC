b121fb5eb74950edf2d69774e08ccce32655ca70
Giovanni Agudinho <giovanniagudinho@gmail.com>
2025-10-05 18:58:43 -0300

    Update Login.js

--- JavaScript/Login.js
+++ JavaScript/Login.js
@@ -1,1 +1,39 @@
+document.addEventListener("DOMContent Loaded", () =>{
+  const form = document.getElementById("formLogin);
+                                       form.addEventListener("submit", async (e) => {
+                                         e.preventDefaault();
+                                         const data = {
+                                           email: form.email.value,
+                                           senha: senha.email.value
+                                         };
+                                         try{
+                                           const res = await fetch("LoginAction.php", {
+                                             method: "POST",
+                                             headers: {"Content-Type":"application/json"},
+                                             body: JSON.stringify(data)
+                                           });
+                                           const json = await res.json();
+                                           if(json.success){
+                                             switch(json.tipo){
+                                               case "Paciente":
+                                                 window.location.href = "HTML/PacientePage.html";
+                                                 break;
+                                               case "Responsável":
+                                                 window.location.href = "HTML/Responsaveis.html";
+                                                 break;
+                                               case "Profissional":
+                                                 window.location.href = "HTML/ProfissionalPage.html";
+                                                 break;
+                                               default:
+                                                 alert("Tipo de usuário desconhecido.");
+                                             }
+                                           }else{
+                                             alert(json.message);
+                                           }
+                                         }catch(err){
+                                           console.error(err);
+                                           alert("Erro na conexão com o servidor.");
+                                         }
+                                       });
+});
 
