712f81049d34da87451032e925b6617ce10d2c94
Giovanni Agudinho <giovanniagudinho@gmail.com>
2025-10-05 18:39:55 -0300

    Update conexao.php

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,1 +1,17 @@
+<?php
+$host = 'localhost';
+$dbname = ''
+$user = ''
+$pass = ''
 
+try{
+  //Cria a conexão PDO
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+
+//Configura o PDO para lançar excessões em caso de erro
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+  die("Erro na conexão: " .$e->getMessage());
+}
+?>
