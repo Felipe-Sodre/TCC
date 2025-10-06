85385775149494ffa974ed92864b9a4930418732
Felipe-Sodre <123928185+Felipe-Sodre@users.noreply.github.com>
2025-10-06 11:45:00 -0300

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,23 +1,5 @@
 //// FELIPE SODRÉ ////
 
-//IR PARA PAGINA LOGIN
-//document.getElementById("pageLogin").addEventListener("click", function() 
-   // {
-    //    window.location.href = "HTML/Login.html";
-   // });
-
-//IR PARA PAGINA CADASTRE-SE
-//document.getElementById("pageCadastro").addEventListener("click", function() 
-   // {
-     //   window.location.href = "HTML/Cadastro.html";
-    //});
-
-//IR PARA PAGINA TRANSTORNOS
-//document.getElementById("pageTranstornos").addEventListener("click", function() 
-   // {
-   //     window.location.href = "HTML/Transtornos.html";
-    //});
-
 //IR PARA A PAGINA INDEX COM O BOTÃO INICIO
 //document.getElementById("btnInicio").addEventListener("click", function() {
   //window.location.href = "index.html";
@@ -32,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "index.html";
     });
 });
+
 
 
 
