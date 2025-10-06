acc701ff8a0554bfb0000f5426079740c7106752
Felipe-Sodre <123928185+Felipe-Sodre@users.noreply.github.com>
2025-10-06 11:42:54 -0300

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -19,10 +19,19 @@
     //});
 
 //IR PARA A PAGINA INDEX COM O BOTÃO INICIO
-document.getElementById("btnInicio").addEventListener("click", function() {
-  window.location.href = "index.html";
-});
+//document.getElementById("btnInicio").addEventListener("click", function() {
+  //window.location.href = "index.html";
+//});
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("btnInicio");
+
+    btn.addEventListener("click", function() {
+        // Vai para a página index.html
+        window.location.href = "index.html";
+    });
+});
 
 
 
