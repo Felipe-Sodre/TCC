b91c40c884e7fcc25cb4961a8f58f1d92da4628c
carlos-07souza <karlo_ssouza@hotmail.com>
2025-11-01 00:04:43 -0300

    Add files via upload
    Arquivo de configuração.

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+ // Configurações de Conexão com o Banco de Dados
+ 
+
+// Host do Banco de Dados 
+define('DB_HOST', 'localhost');
+
+// Nome do Banco de Dados 
+define('DB_NAME', 'neuro_apoio_conecta_bd'); 
+
+// Nome de Usuário 
+define('DB_USER', 'root');
+
+// Senha do MySQL (vazia)
+
+define('DB_PASS', '');
+
+// Charset
+define('DB_CHARSET', 'utf8mb4');
+
+// Modo de Exceção do PDO (para tratamento de erros)
+define('PDO_EXCEPTION_MODE', true);
+?>
